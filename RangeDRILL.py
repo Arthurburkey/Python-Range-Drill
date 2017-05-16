a96ff3317b6c0e70f64cd2a7ca9ef157296fcebb
Arthurburkey <Arthurburkey@email.com>
2017-05-16 00:12:22 -0700

    Range and reverse range functions
    Drills 1 to 3.

---
+++ RangeDRILL.py
@@ -0,0 +1,23 @@
+#STUDENT: Arthur A. Burkey III
+#DRILL: Python range function
+#the Python range function, "range()" generates a list of numbers used to
+#iterate in For loops. The range() function creats a list of index numbers
+#to be used in a loop
+
+
+    
+##Drill 1:
+
+for i in range(4):
+    print(i)
+
+##Drill 2
+for i in reversed(range(4)):
+    #print(i, end=' ')
+   
+
+##Drill 3:
+for i in reversed(range(2,10,2)):
+    print(i, end=' ')
+
+
